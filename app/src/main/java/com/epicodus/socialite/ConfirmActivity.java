2cfc7623dab47f9c59aded7fbc7c93833a7d19c5
Anna Becker <anna.r.becker@gmail.com>
2016-04-21 18:13:01 -0700

    Add confirm activity to display user's event with button to send activity to invitees

--- app/src/main/java/com/epicodus/socialite/ConfirmActivity.java
+++ app/src/main/java/com/epicodus/socialite/ConfirmActivity.java
@@ -1,13 +1,58 @@
 package com.epicodus.socialite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ConfirmActivity extends AppCompatActivity {
+    @Bind(R.id.userEventTextView) TextView mUserEventTextView;
+    @Bind(R.id.userLocationTextView) TextView mUserLocationTextView;
+    @Bind(R.id.userDateTextView) TextView mUserDateTextView;
+    @Bind(R.id.userTimeTextView) TextView mUserTimeTextView;
+    @Bind(R.id.listView) ListView mListView;
+    @Bind(R.id.homeButton) Button mHomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String event = intent.getStringExtra("event");
+        String location = intent.getStringExtra("location");
+        String date = intent.getStringExtra("date");
+        String time = intent.getStringExtra("time");
+        mUserEventTextView.setText(event);
+        mUserLocationTextView.setText("Location: " + location);
+        mUserDateTextView.setText("Date: " + date);
+        mUserTimeTextView.setText("Time: " + time);
+
+        String inviteesString = intent.getStringExtra("inviteeArray");
+        String[] inviteesList = inviteesString.split(", ");
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, inviteesList);
+        mListView.setAdapter(adapter);
+
+
+        mHomeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ConfirmActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                Toast.makeText(ConfirmActivity.this, "your event has been shared", Toast.LENGTH_SHORT).show();
+                startActivity(intent);
+            }
+        });
     }
 }
