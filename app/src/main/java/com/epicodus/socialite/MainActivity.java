22bb990f3a2ae410001e322ea16efdbd3c00a4e4
Anna Becker <anna.r.becker@gmail.com>
2016-04-21 18:08:52 -0700

    Add content to main activity and functionality to make plans button

--- app/src/main/java/com/epicodus/socialite/MainActivity.java
+++ app/src/main/java/com/epicodus/socialite/MainActivity.java
@@ -1,13 +1,29 @@
 package com.epicodus.socialite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.makePlansButton) Button mMakePlansButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mMakePlansButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PlanActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
