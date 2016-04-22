ca9eb5d30f99feadf5c48df1cf4863df24342483
Anna Becker <anna.r.becker@gmail.com>
2016-04-21 22:20:02 -0700

    Add passing test to validate text view content of main activity

---
+++ app/src/test/java/com/epicodus/socialite/MainActivityTest.java
@@ -0,0 +1,34 @@
+package com.epicodus.socialite;
+
+import android.os.Build;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by arbecker on 4/21/16.
+ */
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setup() {
+        activity = Robolectric.setupActivity(MainActivity.class);
+    }
+
+    @Test
+    public void validateTextViewContent() {
+        TextView textView = (TextView) activity.findViewById(R.id.textView);
+        assertTrue("SociaLite".equals(textView.getText().toString()));
+    }
+
+}
