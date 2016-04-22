39a5d5719de9152d42a90504a1b6abea01618ba7
Anna Becker <anna.r.becker@gmail.com>
2016-04-21 22:28:57 -0700

    Add passing test to validate functionality of make plans button

---
+++ app/src/androidTest/java/com/epicodus/socialite/MainActivityInstrumentationTest.java
@@ -0,0 +1,29 @@
+package com.epicodus.socialite;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by arbecker on 4/21/16.
+ */
+public class MainActivityInstrumentationTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void makePlansButtonSendsToCorrectActivity() {
+        onView(withId(R.id.makePlansButton)).perform(click());
+        onView(withId(R.id.textView3)).check(matches
+                (withText("New Event")));
+    }
+}
