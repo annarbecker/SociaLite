bfdf82943462ebda59b88fd6c5846281c535d980
Anna Becker <anna.r.becker@gmail.com>
2016-04-29 10:24:06 -0700

    Remove menu navigation, fix bug with unrecognized resource folder

--- app/src/main/java/com/epicodus/socialite/ui/EventListActivity.java
+++ app/src/main/java/com/epicodus/socialite/ui/EventListActivity.java
@@ -4,10 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.epicodus.socialite.R;
 import com.epicodus.socialite.adapters.EventListAdapter;
@@ -25,8 +21,7 @@ import butterknife.ButterKnife;
 public class EventListActivity extends AppCompatActivity {
     public static final String TAG = EventListActivity.class.getSimpleName();
     public ArrayList<Event> mEvents = new ArrayList<>();
-    @Bind(R.id.recyclerView)
-    RecyclerView mRecyclerView;
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
     private EventListAdapter mAdapter;
 
     String[] allEventNames = new String[] {"Movie Night", "Beyonce Concert", "Brunch", "Hiking"};
@@ -40,13 +35,6 @@ public class EventListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events_list);
         ButterKnife.bind(this);
-
-        setTitle(null);
-
-        Toolbar topToolBar = (Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(topToolBar);
-        topToolBar.setLogo(R.drawable.icon);
-        topToolBar.setLogoDescription(getResources().getString(R.string.logo_desc));
 
         for (int i =0; i<allEventNames.length; i++) {
             Event event = new Event();
@@ -64,33 +52,6 @@ public class EventListActivity extends AppCompatActivity {
         mRecyclerView.setHasFixedSize(true);
         mAdapter = new EventListAdapter(this, mEvents);
         mRecyclerView.setAdapter(mAdapter);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        if(id == R.id.action_refresh){
-            Toast.makeText(EventListActivity.this, "Refresh App", Toast.LENGTH_LONG).show();
-        }
-        if(id == R.id.action_new){
-            Toast.makeText(EventListActivity.this, "Create Text", Toast.LENGTH_LONG).show();
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
 
