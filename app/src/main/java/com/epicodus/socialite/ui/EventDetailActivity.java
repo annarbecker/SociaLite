9eb5d32a3e994faadab7fbef6aed3ced430bb3d9
Anna Becker <anna.r.becker@gmail.com>
2016-04-28 21:42:34 -0700

    Add full functionality to fragment pager adapter

--- app/src/main/java/com/epicodus/socialite/ui/EventDetailActivity.java
+++ app/src/main/java/com/epicodus/socialite/ui/EventDetailActivity.java
@@ -1,7 +1,38 @@
 package com.epicodus.socialite.ui;
 
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import com.epicodus.socialite.R;
+import com.epicodus.socialite.adapters.EventPagerAdapter;
+import com.epicodus.socialite.models.Event;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * Created by arbecker on 4/28/16.
  */
-public class EventDetailActivity {
+public class EventDetailActivity extends AppCompatActivity {
+    @Bind(R.id.viewPager)
+    ViewPager mViewPager;
+    private EventPagerAdapter adapterViewPager;
+    ArrayList<Event> mEvents = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_detail);
+        ButterKnife.bind(this);
+        mEvents = Parcels.unwrap(getIntent().getParcelableExtra("events"));
+        int startingPosition = Integer.parseInt(getIntent().getStringExtra("position"));
+        adapterViewPager = new EventPagerAdapter(getSupportFragmentManager(), mEvents);
+        mViewPager.setAdapter(adapterViewPager);
+        mViewPager.setCurrentItem(startingPosition);
+    }
 }
