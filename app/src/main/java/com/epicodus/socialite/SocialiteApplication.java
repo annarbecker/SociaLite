c954a6e5139c60c089d88b9b754112c841f9992c
Anna Becker <anna.r.becker@gmail.com>
2018-03-18 11:07:51 -0700

    Enable Firebase persistence for offline access

--- app/src/main/java/com/epicodus/socialite/SocialiteApplication.java
+++ app/src/main/java/com/epicodus/socialite/SocialiteApplication.java
@@ -2,7 +2,7 @@ package com.epicodus.socialite;
 
 import android.app.Application;
 
-import com.firebase.client.Firebase;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class SocialiteApplication extends Application {
@@ -10,6 +10,8 @@ public class SocialiteApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Firebase.setAndroidContext(this);
+
+        // enable Firebase persistence for offline access
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     }
 }
